853e96188e55903711487d67e93f3c8c8342141b
Jatin Sodhi <jatinsodhi222@gmail.com>
2021-10-04 13:01:11 +0530

    Java Simplilearn Project

---
+++ src/com/lockedme/LockedMeMain.java
@@ -0,0 +1,10 @@
+package com.lockedme; 
+
+public class LockedMeMain {
+	public static void main(String[] args) {
+		// Create "main" folder if not present in current folder structure
+		FileOperation.createMainFolderIfNotPresent("main");
+		MenuOptions.printWelcomeScreen("LockedMe", "Jatin Sodhi");
+		HandleOperation.handleWelcomeScreenInput(); 
+		}
+}
